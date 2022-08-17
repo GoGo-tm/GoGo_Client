b181350405eb903419631d9222aaa6430a6dc726
JunSeo <oponize@naver.com>
2022-08-17 18:13:14 +0900

    feat: 로딩 처리

---
+++ components/loading.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from 'antd';
+
+const BannerLoading = () => {
+  return (
+    <Skeleton.Image active style={{ width: '100%', height: '21.25rem' }} />
+  );
+};
+
+export { BannerLoading };
