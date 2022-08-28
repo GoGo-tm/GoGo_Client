5e45cb2c073592f2030f6f1bf9aed78f253aedbc
JunSeo <oponize@naver.com>
2022-08-28 11:33:27 +0900

    chore: nextjs proxy 설정

--- next.config.js
+++ next.config.js
@@ -6,39 +6,40 @@ const withAntdLess = require('next-plugin-antd-less');
 
 module.exports = withPlugins(
   [
-    [
-      withPwa,
-      {
-        pwa: {
-          dest: 'public',
-          register: true,
-          skipWaiting: true,
-          disable: process.env.NODE_ENV === 'development',
-        },
+    withPwa({
+      pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === 'development',
       },
-    ],
-    [
-      withAntdLess,
-      {
-        modifyVars: { '@primary-color': '#009D68' },
-        lessVarsFilePath: './assets/css/variables.less',
-        lessVarsFilePathAppendToEndOfContent: false,
-        cssLoaderOptions: {},
-      },
-    ],
+    }),
+    withAntdLess({
+      modifyVars: { '@primary-color': '#009D68' },
+      lessVarsFilePath: './assets/css/variables.less',
+      lessVarsFilePathAppendToEndOfContent: false,
+      cssLoaderOptions: {},
+    }),
   ],
-  [
-    {
-      reactStrictMode: true,
-      webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          use: ['@svgr/webpack'],
-        });
-
-        return config;
-      },
+  {
+    compress: true,
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: '/server/:path*',
+          destination: 'http://3.38.224.88:8080/:path*',
+        },
+      ];
     },
-  ]
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+    },
+  }
 );
