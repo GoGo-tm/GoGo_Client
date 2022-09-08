a0bacc6cc84ff2651d7cc87fd77ca2edfe11bcfb
박준서 <oponize@naver.com>
2022-09-08 13:48:25 +0900

    feat: add typography 컴포넌트

---
+++ components/typography.tsx
@@ -0,0 +1,54 @@
+import React, { PropsWithChildren } from 'react';
+import styled, { css } from 'styled-components';
+
+import contants from '~/constants/theme';
+
+const FONT_WEIGHT = {
+  extraBold: 800,
+  bold: 700,
+  semiBold: 600,
+  medium: 500,
+  regular: 400,
+} as const;
+
+type FontSize = keyof typeof contants.fontSize;
+
+type FontColor = keyof typeof contants.colors;
+
+type FontWeight = keyof typeof FONT_WEIGHT;
+
+interface TypographyProps extends PropsWithChildren {
+  className?: string;
+  size?: FontSize;
+  weight?: FontWeight;
+  color?: FontColor;
+  as?: React.ElementType;
+}
+
+const Typography = ({
+  className,
+  as = 'p',
+  color = 'black',
+  ...props
+}: TypographyProps): React.ReactElement => {
+  return (
+    <BaseTypography as={as} color={color} {...props}>
+      {props.children}
+    </BaseTypography>
+  );
+};
+export default Typography;
+
+const BaseTypography = styled.p<TypographyProps>`
+  ${(props) => css`
+    font-size: ${props.size !== undefined
+      ? `${
+          typeof props.size === 'string'
+            ? contants.fontSize[props.size]
+            : props.size
+        }rem`
+      : ''};
+    font-weight: ${props.weight && FONT_WEIGHT[props.weight]};
+    color: ${props.color};
+  `}
+`;
