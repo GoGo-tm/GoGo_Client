866dfb90318807a392e7dc602e718cf0b088dd73
박준서 <oponize@naver.com>
2023-03-13 18:29:04 +0900

    chore: 날씨예보 로직 수정

--- utils/weather.ts
+++ utils/weather.ts
@@ -2,19 +2,10 @@ import dayjs from 'dayjs';
 
 import { PTY_CONTENTS, PTY_NULL_TYPE, SKY_CONTENTS } from '~/constants/weather';
 
-import * as misc from './misc';
-
 const isBaseTime = ['02', '05', '08', '11', '14', '17', '20', '23'];
 const isBaseTimePlus1Hour = ['03', '06', '09', '12', '15', '18', '21', '00'];
 const isBaseTimePlus2Hour = ['04', '07', '10', '13', '16', '19', '22', '01'];
 const isEdgeTime = ['23', '00', '01'];
-
-interface GetWeather {
-  url: string;
-  apiKey: string;
-  nx: number;
-  ny: number;
-}
 
 interface Item {
   baseDate: string;
@@ -57,26 +48,4 @@ const getPtyOrSkyOfItems = (items: Item[]) => {
   return PTY_CONTENTS[parseInt(getFcstValueOfUniqueItem(items, 'PTY'))];
 };
 
-export const getWeather = async ({ url, apiKey, nx, ny }: GetWeather) => {
-  const [baseDate, baseHour] = getBaseTime();
-
-  try {
-    const res = await fetch(
-      `${url}?serviceKey=${apiKey}&numOfRows=10&pageNo=1&base_date=${baseDate}&base_time=${
-        baseHour + '00'
-      }&nx=${nx}&ny=${ny}&dataType=JSON`
-    );
-
-    const data = await res.json();
-
-    if (data.response.header.resultCode !== '00') {
-      throw misc.getErrorMessage(data.response.header.resultMsg);
-    }
-
-    const weather = getPtyOrSkyOfItems(data.response.body.items.item);
-
-    return weather;
-  } catch (error) {
-    misc.getErrorMessage(error);
-  }
-};
+export { getBaseTime, getPtyOrSkyOfItems };
